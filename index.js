84076b792e37465f5fb8468fac9590ba7b990609
Felipa Mendez <felipalouise@gmail.com>
2022-01-10 23:43:12 -0600

    commit tree 77668ab9b0c5f347d9a300d76dec03238888be00

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(option1 = "roller-skate") {
+    return `This Saturday, I want to ${option1}!`
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(option2 = "go to the office") {
+    return `This Monday, I will ${option2}.`
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(flair="*") {
+     return function (adjective = "special") {
+         return `You are ${flair}${adjective}${flair}!`
+     }
+}
+
+wrapAdjective("||")("a hard worker");
